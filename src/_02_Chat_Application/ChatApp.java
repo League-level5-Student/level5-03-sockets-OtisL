a7a7e50db64a1243771d757157303996398d8a7e
OtisL <42128680+OtisL@users.noreply.github.com>
2020-05-04 15:56:20 -0700

    ChatApp

--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,9 +1,66 @@
 package _02_Chat_Application;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import _00_Click_Chat.gui.ButtonClicker;
+import _00_Click_Chat.networking.Client;
+import _00_Click_Chat.networking.Server;
+
 /*
  * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
  */
 
-public class ChatApp {
-
+public class ChatApp extends JFrame{
+	JTextField tf = new JTextField(20);
+	JPanel panel = new JPanel();
+	JButton button = new JButton("SUBMIT");
+	
+	_02_Chat_Application.Server server;
+	_02_Chat_Application.Client client;
+	
+	
+	public static void main(String[] args) {
+		new ChatApp();
+	}
+	
+	public ChatApp(){
+		
+		int response = JOptionPane.showConfirmDialog(null, "Would you like to host a connection?", "Chat!", JOptionPane.YES_NO_OPTION);
+		if(response == JOptionPane.YES_OPTION){
+			server = new _02_Chat_Application.Server(8080);
+			setTitle("SERVER");
+			JOptionPane.showMessageDialog(null, "Server started at: " + server.getIPAddress() + "\nPort: " + server.getPort());
+			button.addActionListener((e)->{
+				server.sendText(tf.getText());
+			});
+			add(panel);
+			panel.add(tf);
+			panel.add(button);
+			setVisible(true);
+			pack();
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			server.start();
+			
+		}else{
+			setTitle("CLIENT");
+			String ipStr = JOptionPane.showInputDialog("Enter the IP Address");
+			String prtStr = JOptionPane.showInputDialog("Enter the port number");
+			int port = Integer.parseInt(prtStr);
+			client = new _02_Chat_Application.Client(ipStr, port);
+			button.addActionListener((e)->{
+				client.sendText(tf.getText());
+			});
+			add(panel);
+			panel.add(tf);
+			panel.add(button);
+			setVisible(true);
+			pack();
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			client.start();
+		}
+	}
 }
